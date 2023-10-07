39cd0b508566703aca941108441af30c84d4206a
macbook <Gorcevicharis>
2023-10-08 01:10:46 +0200

    rad

--- src/App.js
+++ src/App.js
@@ -1423,6 +1423,7 @@ import Buton from "./importing/buton";
 // // //==========================================================================
 function App() {
   const [inputText, setInputText] = useState([]);
+  function handler() {}
 }
 
 export default App;
