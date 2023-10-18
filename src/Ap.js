eea0af3bd6818ed418fd977d11e2382d1d85e873
macbook <harisgorcevic1000@gmail.com>
2023-10-18 17:56:56 +0200

    vezbanje react router dom

--- src/Ap.js
+++ src/Ap.js
@@ -2,7 +2,18 @@ import React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "./importing";
-
-function Ap() {}
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+function Ap() {
+  return (
+    <div className="container">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>welcome </h1>} />
+          <Route path="/about" element={<h1>This is about PAGE</h1>} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
 
 export default Ap;
